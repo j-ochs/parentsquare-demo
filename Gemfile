92ba60aa978ca9927fb1a74550e9233f0c488b04
Jacob <jochs@westmont.edu>
2018-08-11 16:08:19 -0700

    frontend components rendering

--- Gemfile
+++ Gemfile
@@ -3,5 +3,4 @@ source 'https://rubygems.org'
 gem 'rack'
 
 # gem "rails"
-source 'https://rubygems.org'
 gem 'sinatra'
--- site.rb
+++ site.rb
@@ -1,4 +1,32 @@
 require 'sinatra'
-get '/' do
+
+
+get "/" do
+  @title = "Let Know"
+  @header = "Don't Worry. Report Anonymously."
+  @button_label = "Upload any photos or videos"
+  @text_label = "Describe the incident"
+  @text_placeholder = "Give as much detail as possible. Be specific."
+  @detail_label = "or file a detailed report"
+
+  @greeting = "noVal"
+  @name = "noVal"
   erb :index
+end
+
+
+get '/detail' do
+  erb :detailed_form
+end
+
+post '/detail' do
+  greeting = params[:greeting] || "Hi There"
+  name = params[:name] || "Nobody"
+
+  erb :index, :locals => {'greeting' => greeting, 'name' => name}
+end
+
+
+get "/about" do
+  erb :about
 end
