ba58e4ce252c6e52408c0175b00843cb65874b02
Jacob <jochs@westmont.edu>
2018-08-12 01:49:00 -0700

    starting to come together

--- site.rb
+++ site.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 
-
 get "/" do
   @title = "Let Know"
   @header = "Don't Worry. Report Anonymously."
@@ -8,26 +7,19 @@ get "/" do
   @text_label = "Describe the incident"
   @text_placeholder = "Give as much detail as possible. Be specific."
   @detail_label = "or file a detailed report"
+  @incident_types = ["Alcohol", "Assault", "Bullying", "Hazing", "Drugs", "Graffiti", "Injury", "Suspicious activity",
+              "Trespassing", "Vandalism", "Weapons violation", "Technology misuse", "Other"]
+  @locations = ["On school campus or grounds", "During transportation to/from school", "At school-sponsored events", "Other"]
 
-  @greeting = "noVal"
-  @name = "noVal"
   erb :index
 end
 
 
-get '/detail' do
-  erb :detailed_form
-end
-
 post '/results' do
+  @title = "Let Know"
+  @header = "Don't Worry. Report Anonymously."
   description = params[:description] || "ERROR"
   file = params[:file] || "ERROR"
 
   erb :results, :locals => {'description' => description, 'file' => file}
-end
-
-
-
-get "/about" do
-  erb :about
 end
